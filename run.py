c3e82376621ae872c59580813f3c63996cbcda01
shahin1717 <sahinalekperov5@gmail.com>
2025-11-30 11:25:13 +0400

    added tracking

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         "app.main:app",
-        host="0.0.0.0",
         port=8000,
         reload=False
     )
